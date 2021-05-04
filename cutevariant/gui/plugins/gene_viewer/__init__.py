0be4a897dc7c3c229c188ad48cfdf3fbc42738f0
sacha <sacha@labsquare.org>
2021-05-05 00:51:17 +0200

    [gene viewer] Create a new plugin to display variant along a gene

---
+++ cutevariant/gui/plugins/gene_viewer/__init__.py
@@ -0,0 +1,14 @@
+from PySide2.QtWidgets import QApplication
+
+__title__ = "Gene Viewer"
+__description__ = "A Gene Viewer"
+__long_description__ = QApplication.instance().translate(
+    "gene viewer",
+    """
+<p>This plugin show all variants into a gene viewer</p>
+
+
+""",
+)
+__author__ = "Sacha schutz"
+__version__ = "1.0.0"
